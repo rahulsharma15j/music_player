ab4de498358e36a42fb6f1d916d629bd7e865913
Rahul Sharma <rahulsharma15j@gmail.com>
2022-06-02 21:32:26 +0530

    script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
- 
+const wrapper = document.querySelector(".wrapper"),
+  musicImg = wrapper.querySelector(".img-area img"),
+  musicName = wrapper.querySelector(".song-details .name"),
+  musicArtist = wrapper.querySelector(".song-details .artist");
+
+let musicIndex = 2;
+
+window.addEventListener("load", () => {
+  loadMusic(musicIndex);
+});
+
+function loadMusic(indexNumb) {
+  musicName.innerText = allMusic[indexNumb - 1].name;
+  musicArtist.innerText = allMusic[indexNumb - 1].artist;
+  musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
+}
