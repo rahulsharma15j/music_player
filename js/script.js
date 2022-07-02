b0941e21ec67d70b4cda3d209b1c0c09a12470cd
Rahul Sharma <rahulsharma15j@gmail.com>
2022-07-02 17:06:42 +0530

    m

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ const wrapper = document.querySelector(".wrapper"),
   playPauseBtn = wrapper.querySelector(".play-pause"),
   prevBtn = wrapper.querySelector("#prev"),
   nextBtn = wrapper.querySelector("#next"),
-  progressBar = wrapper.querySelector(".progress-bar");
+  progressArea = wrapper.querySelector(".progress-area");
+progressBar = wrapper.querySelector(".progress-bar");
 
 let musicIndex = 2;
 
@@ -85,4 +86,65 @@ mainAudio.addEventListener("timeupdate", (e) => {
     currentSec = `0${currentSec}`;
   }
   musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+});
+
+progressArea.addEventListener("click", (e) => {
+  let progressWidthval = progressArea.clientWidth;
+  let clickedOffSetX = e.offsetX;
+  let songDuration = mainAudio.duration;
+
+  mainAudio.currentTime = (clickedOffSetX / progressWidthval) * songDuration;
+  playMusic();
+});
+
+//let's work on repeat, shuffle song according to the icon
+const repeatBtn = wrapper.querySelector("#repeat-plist");
+repeatBtn.addEventListener("click", () => {
+  //first we get the innerText of the icon then we'll change accordingly
+  let getText = repeatBtn.innerText; //getting innerText of icon
+  //let's do different changes on different icon click using switch
+  switch (getText) {
+    case "repeat": //if this icon is repeat
+      repeatBtn.innerText = "repeat_one";
+      repeatBtn.setAttribute("title", "Song looped");
+      break;
+    case "repeat_one": //if icon is repeat_one then change it to shuffle
+      repeatBtn.innerText = "shuffle";
+      repeatBtn.setAttribute("title", "Playback shuffled");
+      break;
+    case "shuffle": //if icon is shuffle then change it to repeat
+      repeatBtn.innerText = "repeat";
+      repeatBtn.setAttribute("title", "Playlist looped");
+      break;
+  }
+});
+
+//above we just changed the icon,now let's work on what to do
+//after the song ended
+
+mainAudio.addEventListener("ended", () => {
+  //we'll do according to the icon means if user has set icon to loop song then we'll repeat
+  //the current  song  and will do further accordingly
+
+  let getText = repeatBtn.innerText; //getting innerText of icon
+  //let's do different changes on different icon click using switch
+  switch (getText) {
+    case "repeat": //if this icon is repeat then simply we call the nextMusic function so that next song will be played
+      nextMusic();
+      break;
+    case "repeat_one": //if icon is repeat_one then we'll change the current playing song current time to 0 so song will play from beginning
+      mainAudio.currentTime = 0;
+      loadMusic(indexNumb);
+      break;
+    case "shuffle": //if icon is shuffle then change it to repeat
+      //generating random index between the max range of array length
+      let randIndex = Math.floor(Math.random() * allMusic.length + 1);
+      do {
+        randIndex = Math.floor(Math.random() * allMusic.length + 1);
+      } while ((musicIndex = randIndex)); //this loop run until the next random number won't be the same of current music index
+      musicIndex = randIndex; //passing randomIndex to musicIndex so the random song will play
+      loadMusic(musicIndex); //calling loadMusic function
+      playMusic(); //calling playMusic function
+      break;
+  }
 });
